0983fa2e4703bec4591a1e01c11a0c98612ad9e5
杰枫 <huanggbxjp@sohu.com>
2017-09-20 11:20:33 +0800

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+﻿var Express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var app = Express();
+app.use(bodyParser.json());
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+var upload = multer({ storage: Storage }).array('imgfile',40); 
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/upload", function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+});
+
+app.listen(2000, function (a) {
+    console.log("Listening to port 2000");
+});
